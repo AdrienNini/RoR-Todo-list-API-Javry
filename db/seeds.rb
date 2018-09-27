51eb6f1127ca413355c4c5ab9b9b02f46bec8504
Adrien Nini Pereira <adrien97nini@gmail.com>
2018-09-27 02:16:25 +0200

    Add seed data to populate the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.create(title: "Code a todo list app", done: true, created_at: DateTime.now, done_at: DateTime.now)
+Todo.create(title: "Upload it", done: true, created_at: DateTime.now, done_at: DateTime.now)
+Todo.create(title: "Pass the test", done: false, created_at: DateTime.now, done_at: nil)
+Todo.create(title: "Get the job !", done: false, created_at: DateTime.now, done_at: nil)
+
+
